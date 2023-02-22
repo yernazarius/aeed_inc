47308f02bbe5ef56108d477d2eca9cf5aa6414dd
Oskr11 <121083865+Oskr11@users.noreply.github.com>
2023-02-22 15:20:11 +0600

    Delete src directory

--- src/db/i_db.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface i_db {
-    Connection getConnection() throws ClassNotFoundException, SQLException;
-
-}
